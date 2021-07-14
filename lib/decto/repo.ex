717f13b749022633d692f0296c5c9f1f0bcf24aa
Vinai Rachakonda <rachakondavinai@gmail.com>
2021-07-14 16:11:48 -0700

    can start link and commit

--- lib/decto/repo.ex
+++ lib/decto/repo.ex
@@ -3,3 +3,28 @@ defmodule Decto.Repo do
     otp_app: :ecto_blog,
     adapter: Ecto.Adapters.MyXQL
 end
+
+defmodule Decto.Person do
+  use Ecto.Schema
+
+  schema "people" do
+    field :first_name, :string
+    field :last_name, :string
+    field :age, :integer
+  end
+end
+
+
+defmodule Decto.App do
+  # Decto.Repo.start_link()
+  alias Decto.{Repo, Person}
+
+  def commit(message) do
+    Repo.query!("SELECT DOLT_COMMIT('-a', '-m', '#{message}');")
+  end
+
+  def insertPerson(first_name, last_name, age) do
+    person = %Person{first_name: first_name, last_name: last_name, age: age}
+    Repo.insert(person)
+  end
+end
